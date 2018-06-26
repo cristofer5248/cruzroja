8ef23a4f10d0ecca2f3aa7596220a75854463e0a
cristofer5248 <cristofer5248@cristofer5248-Q302LAB>
2018-06-25 20:09:56 -0600

    Arreglando generador de ID y poniendo que se exporte en pdf y excel las tablas :D

--- src/java/sv/cruzroja/www/entities/DepartamentoEntity.java
+++ src/java/sv/cruzroja/www/entities/DepartamentoEntity.java
@@ -11,6 +11,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -30,7 +32,9 @@ import javax.xml.bind.annotation.XmlTransient;
     @NamedQuery(name = "DepartamentoEntity.findByNombre", query = "SELECT d FROM DepartamentoEntity d WHERE d.nombre = :nombre")})
 public class DepartamentoEntity implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "iddepartamento", nullable = false)
     private Integer iddepartamento;
--- src/java/sv/cruzroja/www/utils/Verificarsesion.java
+++ src/java/sv/cruzroja/www/utils/Verificarsesion.java
@@ -62,7 +62,7 @@ public class Verificarsesion {
 
     }
 
-    public void verificarsession2()throws IOException {
+    public void verificarsession2() throws IOException {
         try {
 
             FacesContext context = FacesContext.getCurrentInstance();
@@ -94,7 +94,45 @@ public class Verificarsesion {
             }
 
         } catch (Exception e) {
-            
+
+            FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml?error=true");
+            JsfUtil.setErrorMessage(null, "Error interno");
+        }
+    }
+
+    public void verificarsession3() throws IOException {
+        try {
+
+            FacesContext context = FacesContext.getCurrentInstance();
+            UsuariosEntity us = (UsuariosEntity) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("nivel3");
+//        UsuariosEntity usuario = new UsuariosEntity();
+            String nombre = null;
+
+            if (us == null) {
+
+                FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml?error=true");
+            } else {
+                //System.out.println("Hasta aqui el estado es: " + us.getActivo().toString()+" en el metodo de veriicarusuario2");
+                if (us.getActivo() == false) {
+                    nombre = us.getNombres();
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido Tecnico", nombre));
+                    UsuariosModel model = new UsuariosModel();
+                    UsuariosEntity cambiaractivo = new UsuariosEntity();
+                    cambiaractivo.setIdusuario(us.getIdusuario());
+                    cambiaractivo.setNombres(us.getNombres());
+                    cambiaractivo.setPass(us.getPass());
+                    cambiaractivo.setTipousuario(us.getTipousuario());
+                    cambiaractivo.setGenero(us.getGenero());
+                    cambiaractivo.setActivo(Boolean.TRUE);
+                    model.modificarUsuarios(cambiaractivo);
+                    us.setActivo(Boolean.TRUE);
+                    this.setMensaje(us.getNombres());
+                }
+
+            }
+
+        } catch (Exception e) {
+
             FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml?error=true");
             JsfUtil.setErrorMessage(null, "Error interno");
         }
@@ -108,12 +146,12 @@ public class Verificarsesion {
             UsuariosEntity us = (UsuariosEntity) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
             return us.getNombres();
         } catch (Exception e) {
-            
+
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Error interno ", "Al obtener session"));
 
         }
         return null;
-        
+
     }
 
     /**
